66494d298647b684190bf5f31ec117b65614bca1
rishipandey2 <rishipandeyinfo@gmail.com>
2024-01-02 07:48:44 +0530

    Added new components

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,12 +1,13 @@
 
 
-
+import Rishi from "./Rishi"
 function App() {
   
 
   return (
     <>
   <h1>Rishi is learning React</h1>
+  <Rishi/>
     </>
   )
 }
---
+++ 01vitereact/src/Rishi.jsx
@@ -0,0 +1,7 @@
+function Rishi(){
+    return(
+        <h3>Rishi is ready to make this year highly productive.</h3>
+    )
+}
+
+export default Rishi
