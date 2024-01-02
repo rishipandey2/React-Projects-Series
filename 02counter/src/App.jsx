2e1eeb4c64a407dc9ea12dcf996df57bc4455474
rishipandey2 <rishipandeyinfo@gmail.com>
2024-01-02 08:19:13 +0530

    Added new project

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+ const [counter, setCounter] =  useState(0)
+ 
+
+const addValue = ()=>{
+  setCounter(counter+1)
+}
+
+const removeValue = () =>{
+  setCounter(counter-1)
+
+  
+}
+
+const resetValue = ()=>{
+  setCounter(0)
+}
+  return (
+  <>
+  <h1>Counter</h1>
+  <h2>Counter value : {counter}</h2>
+
+  <button
+  onClick={addValue}
+  >Add value</button>
+  <br></br>
+  <br></br>
+  <button 
+  onClick={removeValue}
+  >Remove value</button>
+  <br></br>
+  <br></br>
+  <button
+  onClick={resetValue}
+  >Reset value</button>
+  </>
+  )
+}
+
+export default App
