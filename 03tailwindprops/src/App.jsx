45abf7a378f6ef99f2834d02dcfa3e414d653dd1
rishipandey2 <rishipandeyinfo@gmail.com>
2024-01-04 09:26:43 +0530

    Added new project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-green-400 text-black p-4 rounded-xl'>Tailwind Test</h1>
+
+<Card name = "Rinku" text = "Show me"/>
+
+<br></br>
+
+<Card name = "Rishi" text = "Click Here"/>
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ 03tailwindprops/src/components/Card.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function Card({name, text}){
+    return(
+        <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+        <img className="w-24 h-24 rounded-full mx-auto" src="https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="" width="384" height="512"/>
+        <div className="pt-6 md:p-8 text-center space-y-4">
+          <blockquote>
+            <p className="text-lg font-medium">
+              “Tailwind CSS is the only framework that I've seen scale
+              on large teams. It’s easy to customize, adapts to any design,
+              and the build size is tiny.”
+            </p>
+          </blockquote>
+          <figcaption className="font-medium">
+            <div className="text-sky-500 dark:text-sky-400">
+              {name}
+            </div>
+            <div className="text-slate-700 dark:text-slate-500">
+              {text}
+            </div>
+          </figcaption>
+      
+      
+      
+        </div>
+      </figure>
+
+
+
+    )
+}
+
+export default Card
